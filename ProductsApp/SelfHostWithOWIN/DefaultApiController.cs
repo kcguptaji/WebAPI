79f861c5fe11dfbd4d334c2530303209a74229b8
Komal Chandra Gupta <komal.chandra.gupta@jci.com>
2017-12-02 13:45:39 -0600

    - Add APISelfHosting project using owin over console - Add product controllers, model, index.html

---
+++ ProductsApp/SelfHostWithOWIN/DefaultApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web.Http;
+
+namespace SelfHostWithOWIN
+{
+    public class DefaultApiController : ApiController
+    {
+        // api/Get
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"DotNet", "MVC", "C#"};
+        }
+
+        // api/Get/id
+        public string Get(int id)
+        {
+            return "the value is " + id.ToString();
+        }
+
+
+    }
+}
---
+++ ProductsApp/SelfHostWithOWIN/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.Owin.Hosting;
+
+namespace SelfHostWithOWIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var baseUrl = "http://localhost:1579/";
+
+            using (WebApp.Start<Startup>(url: baseUrl))
+            {
+                HttpClient client = new HttpClient();
+                var resp = client.GetAsync(baseUrl).Result;
+
+                Console.WriteLine(resp);
+                Console.WriteLine(resp.Content.ReadAsStringAsync().Result);
+
+                Console.ReadLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
